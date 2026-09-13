4c32afae52140d9bc9a5446baa8579c9e288bf17
vijaybadsila <145692160+vijaybadsila@users.noreply.github.com>
2026-09-13 20:30:23 +0530

    Refactor clone function to improve logging and syntax

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
-def clone(String url, String branch) {
-    echo "This is the cloning step"
-    git url: url, branch: branch
+def call(String url, String branch) {
+    echo "Cloning repository: ${url} (branch: ${branch})"
+    git url: url, branch: "*/${branch}"
     echo "Cloned successfully"
 }
